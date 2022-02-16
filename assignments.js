4a842c1484c8e189a1cd0ec167ff7f0069e53937
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-16 14:40:10 -0800

    complete lecture 13

--- assignments.js
+++ assignments.js
@@ -1,7 +1,7 @@
-let country = 330000000;
-let continent = 579000000;
-let population = 8000000000;
+// let country = 330000000;
+// let continent = 579000000;
+// let population = 8000000000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
--- scripts.js
+++ scripts.js
@@ -1,10 +1,30 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-console.log("jonas");
-console.log(23);
+// console.log("jonas");
+// console.log(23);
 
-let firstName = "justice";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "justice";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'justice');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1995;
+console.log(typeof year);
+
+console.log(typeof null);
