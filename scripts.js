2930d815d87358f9a96f002fb657ad039018bc17
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-16 15:39:59 -0800

    complete lecture 14

--- scripts.js
+++ scripts.js
@@ -9,22 +9,66 @@
 // console.log(firstName);
 // console.log(firstName);
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-// console.log(typeof true);
-console.log(typeof javascriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof 'justice');
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'justice');
 
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1995;
-console.log(typeof year);
+// year = 1995;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1995;
+// // birthYear = 1990;
+
+// // const job;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// let lastName = 'douglas';
+// console.log(lastName);
+
+// Math Operators
+const now = 2037;
+const ageJustice = now - 1995;
+const ageAmahla = now - 1997;
+console.log(ageJustice, ageAmahla);
+
+console.log(ageJustice * 2, ageJustice / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Justice";
+const lastName = "Pelteir";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operators
+console.log(ageJustice > ageAmahla); // >, <, >=, <=
+console.log(ageAmahla >= 18);
+
+const isFullAge = ageAmahla >= 18;
+
+console.log(now - 1991 > now - 2018);
