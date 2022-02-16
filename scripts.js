adae19d87ad2fd24f0f724ee3a0dc755f4717899
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-16 15:58:03 -0800

    complete lecture 15

--- scripts.js
+++ scripts.js
@@ -44,31 +44,44 @@
 // console.log(lastName);
 
 // Math Operators
+// const now = 2037;
+// const ageJustice = now - 1995;
+// const ageAmahla = now - 1997;
+// console.log(ageJustice, ageAmahla);
+
+// console.log(ageJustice * 2, ageJustice / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "Justice";
+// const lastName = "Pelteir";
+// console.log(firstName + " " + lastName);
+
+// // Assignment Operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// x--; // x = x - 1
+// console.log(x);
+
+// // Comparison operators
+// console.log(ageJustice > ageAmahla); // >, <, >=, <=
+// console.log(ageAmahla >= 18);
+
+// const isFullAge = ageAmahla >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
 const now = 2037;
 const ageJustice = now - 1995;
 const ageAmahla = now - 1997;
-console.log(ageJustice, ageAmahla);
-
-console.log(ageJustice * 2, ageJustice / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
-
-const firstName = "Justice";
-const lastName = "Pelteir";
-console.log(firstName + " " + lastName);
-
-// Assignment Operators
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
-x--; // x = x - 1
-x--; // x = x - 1
-console.log(x);
-
-// Comparison operators
-console.log(ageJustice > ageAmahla); // >, <, >=, <=
-console.log(ageAmahla >= 18);
-
-const isFullAge = ageAmahla >= 18;
 
 console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJustice + ageAmahla) / 2;
+console.log(ageJustice, ageAmahla, averageAge);
