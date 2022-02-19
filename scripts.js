833514d83f1f78f1dc896a4fbf10ceae00b6beda
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-19 15:21:53 -0800

    complete lecture 21

--- scripts.js
+++ scripts.js
@@ -199,20 +199,42 @@ Hint: Use an if/else statement
 
 ///////////////////////////////
 
-const inputYear = `1991`;
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// const inputYear = `1991`;
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number(`Justice`));
-console.log(typeof NaN);
+// console.log(Number(`Justice`));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-// type coercion
-console.log(`I am ` + 23 + ` years old`);
-console.log(`23` - `10` - 3);
-console.log(`23` * `2`);
+// // type coercion
+// console.log(`I am ` + 23 + ` years old`);
+// console.log(`23` - `10` - 3);
+// console.log(`23` * `2`);
 
-let n = `1` + 1;
-n = n - 1;
-console.log(n);
+// let n = `1` + 1;
+// n = n - 1;
+// console.log(n);
+
+// 5 falsy values: 0, ', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Justice'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 10;
+if(money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("You should get a job")
+}
+
+let height = 23;
+if(height) {
+  console.log(`YAY! Height is defined.`);
+} else {
+  console.log(`Height is undefined!`);
+}
