297eac7ece31ee3776fe4ace9fbb2c40be15d936
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-19 15:57:10 -0800

    complete lecture 22

--- scripts.js
+++ scripts.js
@@ -219,22 +219,44 @@ Hint: Use an if/else statement
 
 // 5 falsy values: 0, ', undefined, null, NaN
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Justice'));
-console.log(Boolean({}));
-console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Justice'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
 
-const money = 10;
-if(money) {
-  console.log("Don't spend it all");
-} else {
-  console.log("You should get a job")
+// const money = 10;
+// if(money) {
+//   console.log("Don't spend it all");
+// } else {
+//   console.log("You should get a job")
+// }
+
+// let height = 23;
+// if(height) {
+//   console.log(`YAY! Height is defined.`);
+// } else {
+//   console.log(`Height is undefined!`);
+// }
+
+const age = 18;
+if(age === 18) console.log(`You just became an adult (strict).`);
+
+if(age == 18) console.log(`You just became an adult (loose).`);
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite  === 23) {
+  console.log('Cool! 23 is an amazing number');
+} else if(favorite === 7) {
+  console.log('7 is also a cool number');
+} else if(favorite === 9) {
+  console.log('9 is also a cool number')
+}
+else {
+  console.log("Number is not 23 or 7");
 }
 
-let height = 23;
-if(height) {
-  console.log(`YAY! Height is defined.`);
-} else {
-  console.log(`Height is undefined!`);
-}
+if (favorite !== 23) console.log("why not 23?");
