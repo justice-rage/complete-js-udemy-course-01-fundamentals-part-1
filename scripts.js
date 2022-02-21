605086ed3dfa5c19a6a1a060a8bc469e58c4d69e
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-20 19:07:36 -0800

    complete lecture 28

--- scripts.js
+++ scripts.js
@@ -386,12 +386,28 @@ Test Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106.
 //   console.log('not a valid date1');
 // }
 
-3 + 4
-1991
-true && false && !false
+// 3 + 4
+// 1991
+// true && false && !false
 
-if (23 > 10) {
-  const str = '23 is bigger';
+// if (23 > 10) {
+//   const str = '23 is bigger';
+// }
+
+// console.log(`I'm ${2037 - 1991} years old`)
+
+const age = 17;
+// age >= 18 ? console.log(`I like to drink wine 🍷`) : console.log(`I like to drink water 💧`);
+
+const drink = age >= 18 ? `wine 🍷` : `water 💧`;
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = `wine 🍷`;
+} else {
+  drink2 = `water 💧`;
 }
+console.log(drink2);
 
-console.log(`I'm ${2037 - 1991} years old`)
+console.log(`I like to drink ${age >= 18 ? `wine 🍷` : `water 💧`}`)
